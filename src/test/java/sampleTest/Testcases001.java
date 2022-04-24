a784dc2c3dc33255a15f1ceecce7fe84aeb8417c
snjaisky <snjaisky@gmail.com>
2022-04-24 22:36:49 +0530

    jenkins practise project

---
+++ src/test/java/sampleTest/Testcases001.java
@@ -0,0 +1,29 @@
+package sampleTest;
+
+import org.testng.annotations.Test;
+
+public class Testcases001 {
+	
+	@Test
+	public static void Testcase_001() {
+		System.out.println( "This is a sample testcase of 001 ");
+	}
+	
+	@Test
+	public static void Testcase_002() {
+		System.out.println( "This is a sample testcase of 002 ");
+	}
+	
+	
+	@Test
+	public static void Testcase_003() {
+		System.out.println( "This is a sample testcase of 003 ");
+	}
+	
+	
+	@Test
+	public static void Testcase_004() {
+		System.out.println( "This is a sample testcase of 004 ");
+	}
+
+}
---
+++ src/test/java/sampleTest/Testcases002.java
@@ -0,0 +1,30 @@
+package sampleTest;
+
+import org.testng.annotations.Test;
+
+public class Testcases002 {
+	
+	@Test
+	public static void Testcase_005() {
+		System.out.println( "This is a sample testcase of 005 ");
+	}
+	
+	@Test
+	public static void Testcase_006() {
+		System.out.println( "This is a sample testcase of 006 ");
+	}
+	
+	
+	@Test
+	public static void Testcase_007() {
+		System.out.println( "This is a sample testcase of 007 ");
+	}
+	
+	
+	@Test
+	public static void Testcase_00() {
+		System.out.println( "This is a sam8ple testcase of 008 ");
+	}
+
+
+}
